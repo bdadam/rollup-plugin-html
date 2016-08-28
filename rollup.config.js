5140034af5f1181e5f12650bab5ee074156487cd
bdadam <me@bdadam.com>
2016-08-28 14:23:29 +0200

    specified external modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import buble from 'rollup-plugin-buble';
 
 var pkg = require('./package.json');
+var external = Object.keys(pkg.dependencies).concat('path');
 
 export default {
 	entry: 'index.js',
 	plugins: [buble()],
+	external: external,
 	targets: [
 		{
 			format: 'cjs',
